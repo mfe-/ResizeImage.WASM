e2eb6f89468bc6dde286704c48a2e45d74d4262d
Martin Fetzel <martin.fetzel@get-the-solution.net>
2021-04-30 18:49:35 +0200

    images

---
+++ ResizeImage.WASM/ImageFile.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResizeImage.WASM
+{
+    public class ImageFile : IDisposable, INotifyPropertyChanged
+    {
+        protected Func<ImageFile, Stream> _openStreamFunction;
+        public ImageFile(string path, Stream stream)
+        {
+            Path = path;
+            Stream = stream;
+        }
+        public ImageFile(string path, Stream stream, int width, int height) : this(path, stream)
+        {
+            Width = width;
+            Height = height;
+        }
+        public ImageFile(string path, Stream stream, int width, int height, FileInfo fileInfo) :
+            this(path, stream, width, height)
+        {
+            FileInfo = fileInfo;
+        }
+        public ImageFile(string path, int width, int height, FileInfo fileInfo, Func<ImageFile, Stream> funcStreamCallBack) :
+            this(path, null, width, height)
+        {
+            FileInfo = fileInfo;
+            _openStreamFunction = funcStreamCallBack;
+        }
+
+        public String Name
+        {
+            get { return FileInfo?.Name; }
+        }
+
+        //
+        // Summary:
+        //     Gets the full file-system path of the item, if the item has a path.
+        //
+        // Returns:
+        //     The full path of the item, if the item has a path in the user's file-system.
+        public string Path { get; set; }
+
+        private Stream _Stream;
+
+        public Stream Stream
+        {
+            get
+            {
+                if (_Stream == null && _openStreamFunction != null)
+                {
+                    Stream = _openStreamFunction(this);
+                }
+                return _Stream;
+            }
+            set
+            {
+                SetProperty(ref _Stream, value, nameof(Stream));
+            }
+        }
+
+        protected int _Width;
+        public int Width
+        {
+            get { return _Width; }
+            set { SetProperty(ref _Width, value, nameof(Width)); }
+        }
+
+        protected int _Height;
+        public int Height
+        {
+            get { return _Height; }
+            set { SetProperty(ref _Height, value, nameof(Height)); }
+        }
+
+        protected int _NewWidth;
+        public int NewWidth
+        {
+            get { return _NewWidth; }
+            set { SetProperty(ref _NewWidth, value, nameof(NewWidth)); }
+        }
+
+        protected int _NewHeight;
+        public int NewHeight
+        {
+            get { return _NewHeight; }
+            set { SetProperty(ref _NewHeight, value, nameof(NewHeight)); }
+        }
+
+        public FileInfo FileInfo { get; set; }
+
+        public object Tag { get; set; }
+
+        /// <summary>
+        /// Get or sets whether the image file could not be modified (an other proccess has opened the file already, or no write permissions)
+        /// </summary>
+        protected bool _IsReadOnly;
+        public bool IsReadOnly
+        {
+            get { return _IsReadOnly; }
+            set { SetProperty(ref _IsReadOnly, value, nameof(IsReadOnly)); }
+        }
+
+        public DateTimeOffset LastModified { get; set; }
+
+        public long Size => Stream.Length;
+
+        public string ContentType { get; set; }
+
+        public string ToBase64ImageSrc()
+        {
+            return $"data:{ (object)ContentType};base64, { Convert.ToBase64String((Stream as MemoryStream)?.ToArray() ?? new byte[0])}";
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Cleanup
+            Stream?.Dispose();
+        }
+    }
+}
--- ResizeImage.WASM/Pages/IndexComponent.cs
+++ ResizeImage.WASM/Pages/IndexComponent.cs
@@ -2,15 +2,17 @@
 using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ResizeImage.WASM.Pages
 {
-    public class IndexComponent : ComponentBase, IDisposable
+    public class IndexComponent : ComponentBase, IDisposable, INotifyPropertyChanged
     {
-        protected Dictionary<IBrowserFile, MemoryStream> loadedFiles = new Dictionary<IBrowserFile, MemoryStream>();
         protected long maxFileSize = 5 * 1000 * 100;
         protected int maxAllowedFiles = 3;
         protected bool isLoading;
@@ -23,14 +25,15 @@ namespace ResizeImage.WASM.Pages
 
         private void UnloadFiles()
         {
-            foreach (var item in loadedFiles)
+            foreach (var item in ImageFiles)
             {
-                item.Value.Dispose();
+                item?.Stream?.Dispose();
+                (item?.Tag as SixLabors.ImageSharp.Image)?.Dispose();
             }
-            loadedFiles.Clear();
+            ImageFiles.Clear();
         }
 
-        protected async Task LoadFiles(InputFileChangeEventArgs e)
+        protected async Task OpenFilePicker(InputFileChangeEventArgs e)
         {
             isLoading = true;
             UnloadFiles();
@@ -38,6 +41,7 @@ namespace ResizeImage.WASM.Pages
 
             try
             {
+                ImageFiles = new ObservableCollection<ImageFile>();
                 foreach (var file in e.GetMultipleFiles(maxAllowedFiles))
                 {
                     Stream s = file.OpenReadStream(maxFileSize);
@@ -45,7 +49,16 @@ namespace ResizeImage.WASM.Pages
                     await s.CopyToAsync(memoryStream);
                     s.Close();
                     s.Dispose();
-                    loadedFiles.Add(file, memoryStream);
+
+                    memoryStream.Position = 0;
+                    var i = await SixLabors.ImageSharp.Image.LoadAsync(memoryStream);
+                    ImageFile imageFile = new ImageFile(file.Name, memoryStream, i.Width, i.Height)
+                    {
+                        ContentType = file.ContentType,
+                        Tag = i
+                    };
+
+                    ImageFiles.Add(imageFile);
                 }
             }
             catch (Exception ex)
@@ -54,6 +67,65 @@ namespace ResizeImage.WASM.Pages
             }
 
             isLoading = false;
+        }
+        public string result = "";
+        protected Task OnResizeButtonClick()
+        {
+            result = "asdf";
+            return Task.CompletedTask;
+        }
+
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Get or sets the list of files which should be resized
+        /// </summary>
+        private ObservableCollection<ImageFile> _ImageFiles = new ObservableCollection<ImageFile>();
+        public ObservableCollection<ImageFile> ImageFiles
+        {
+            get { return _ImageFiles; }
+            set
+            {
+                //SetProperty(ref _ImageFiles, value, nameof(ImageFiles));
+                if (ImageFiles != null)
+                {
+                    ImageFiles.CollectionChanged += ImageFiles_CollectionChanged;
+                }
+                //ApplyPreviewDimensions();
+                //OnPropertyChanged(nameof(ShowOpenFilePicker));
+                //OnPropertyChanged(nameof(SingleFile));
+            }
+        }
+        public bool ShowOpenFilePicker
+        {
+            get { return ImageFiles != null && ImageFiles.Count == 0; }
+        }
+        private void ImageFiles_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            //OnPropertyChanged(nameof(ShowOpenFilePicker));
+            //OnPropertyChanged(nameof(CanOverwriteFiles));
+            //OnPropertyChanged(nameof(SingleFile));
+            if (!_ImageFiles.Any())
+            {
+                OnPropertyChanged(nameof(ImageFiles));
+            }
+            //ApplyPreviewDimensions();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual bool SetProperty<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 }
